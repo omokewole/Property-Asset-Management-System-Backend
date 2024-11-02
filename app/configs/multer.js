34e5c7c261cb0442a41a707dacd3e2482e1accab
demandtech <demandwork247@gmail.com>
2024-11-02 20:34:53 +0100

    directory not found fix

--- app/configs/multer.js
+++ app/configs/multer.js
@@ -1,13 +1,20 @@
 import multer from "multer";
+import fs from "fs";
+
+const uploadsDir = "uploads/";
+
+if (!fs.existsSync(uploadsDir)) {
+	fs.mkdirSync(uploadsDir);
+}
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
+	destination: (req, file, cb) => {
+		cb(null, "uploads/");
+	},
+	filename: (req, file, cb) => {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		cb(null, uniqueSuffix + "-" + file.originalname);
+	},
 });
 
 const upload = multer({ storage });
