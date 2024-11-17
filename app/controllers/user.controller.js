0fdda7f62bd7a532a92d161fb5e80fa2bbf78503
rasheed <demandwork247@gmail.com>
2024-11-17 07:17:18 +0100

    user property performance reports

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,12 +1,12 @@
 import {
-	createUser,
-	loginUser,
-	user,
-	verifyUserEmail,
-	updateUser,
-	changePassword,
-	updatedSettings,
-	userReport,
+  createUser,
+  loginUser,
+  user,
+  verifyUserEmail,
+  updateUser,
+  changePassword,
+  updatedSettings,
+  userPropertyPerformanceReport,
 } from "../services/user.service.js";
 import { responseModel } from "../utils/responseModel.js";
 import { refreshToken } from "../utils/generateToken.js";
@@ -19,187 +19,197 @@ import fs from "fs";
 const unlinkAsync = promisify(fs.unlink);
 
 export async function handleCreateUser(req, res) {
-	try {
-		const newUserData = req.body;
+  try {
+    const newUserData = req.body;
 
-		const result = await createUser(newUserData);
+    const result = await createUser(newUserData);
 
-		await sendVerificationMail(result.user);
+    await sendVerificationMail(result.user);
 
-		const user = result.user;
+    const user = result.user;
 
-		delete user.email_token;
+    delete user.email_token;
 
-		result.user = user;
+    result.user = user;
 
-		res
-			.status(201)
-			.json(responseModel(true, "User created successfully", result));
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured"));
-	}
+    res
+      .status(201)
+      .json(responseModel(true, "User created successfully", result));
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured"));
+  }
 }
 
 export async function handleLoginUser(req, res) {
-	try {
-		const loginInfo = req.body;
+  try {
+    const loginInfo = req.body;
 
-		const result = await loginUser(loginInfo);
+    const result = await loginUser(loginInfo);
 
-		res.status(200).json(responseModel(true, "Login succesffuly", result));
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured", null));
-	}
+    res.status(200).json(responseModel(true, "Login succesffuly", result));
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured", null));
+  }
 }
 
 export async function handleVerifyUser(req, res) {
-	try {
-		const { email_token } = req.body;
+  try {
+    const { email_token } = req.body;
 
-		const result = await verifyUserEmail(email_token);
+    const result = await verifyUserEmail(email_token);
 
-		sendConfirmationMail(result.user);
+    sendConfirmationMail(result.user);
 
-		res
-			.status(200)
-			.json(responseModel(true, "User email verified successfully", result));
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured", null));
-	}
+    res
+      .status(200)
+      .json(responseModel(true, "User email verified successfully", result));
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured", null));
+  }
 }
 
 export async function handleUser(req, res) {
-	try {
-		const authUser = req.user;
+  try {
+    const authUser = req.user;
 
-		const result = await user(authUser._id);
+    const result = await user(authUser._id);
 
-		res
-			.status(200)
-			.json(responseModel(true, "User fetched successfully", result));
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured", null));
-	}
+    res
+      .status(200)
+      .json(responseModel(true, "User fetched successfully", result));
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured", null));
+  }
 }
 
 export async function handleRefreshToken(req, res) {
-	try {
-		const { refresh_token } = req.body;
+  try {
+    const { refresh_token } = req.body;
 
-		const { newAccessToken, newRefreshToken } = await refreshToken(
-			refresh_token
-		);
+    const { newAccessToken, newRefreshToken } = await refreshToken(
+      refresh_token
+    );
 
-		res.status(201).json(
-			responseModel(true, "New token generated successfull", {
-				access_token: newAccessToken,
-				refresh_token: newRefreshToken,
-			})
-		);
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured", null));
-	}
+    res.status(201).json(
+      responseModel(true, "New token generated successfull", {
+        access_token: newAccessToken,
+        refresh_token: newRefreshToken,
+      })
+    );
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured", null));
+  }
 }
 
 export async function handleUpdateUser(req, res) {
-	try {
-		const userInfo = req.body;
-		const userId = req.user._id;
-		const file = req.file;
+  try {
+    const userInfo = req.body;
+    const userId = req.user._id;
+    const file = req.file;
 
-		if (file) {
-			try {
-				const uploadResult = await cloudinary.uploader.upload(file.path, {
-					folder: "profile_photo",
-				});
+    if (file) {
+      try {
+        const uploadResult = await cloudinary.uploader.upload(file.path, {
+          folder: "profile_photo",
+        });
 
-				userInfo.image_url = uploadResult.secure_url;
-			} catch (error) {
-				return res
-					.status(500)
-					.json(
-						responseModel(
-							false,
-							"Image upload failed. Property not saved.",
-							error
-						)
-					);
-			} finally {
-				await unlinkAsync(file.path);
-			}
-		}
+        userInfo.image_url = uploadResult.secure_url;
+      } catch (error) {
+        return res
+          .status(500)
+          .json(
+            responseModel(
+              false,
+              "Image upload failed. Property not saved.",
+              error
+            )
+          );
+      } finally {
+        await unlinkAsync(file.path);
+      }
+    }
 
-		const result = await updateUser(userInfo, userId);
+    const result = await updateUser(userInfo, userId);
 
-		res
-			.status(200)
-			.json(responseModel(true, "User updated successfully", result));
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured", null));
-	}
+    res
+      .status(200)
+      .json(responseModel(true, "User updated successfully", result));
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured", null));
+  }
 }
 
 export async function handleChangePassword(req, res) {
-	try {
-		const { current_password, new_password } = req.body;
-		const userId = req.user._id;
+  try {
+    const { current_password, new_password } = req.body;
+    const userId = req.user._id;
 
-		await changePassword({
-			currentPassword: current_password,
-			newPassword: new_password,
-			userId,
-		});
+    await changePassword({
+      currentPassword: current_password,
+      newPassword: new_password,
+      userId,
+    });
 
-		res.status(200).json(responseModel(true, "Password successfully changed!"));
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured!"));
-	}
+    res.status(200).json(responseModel(true, "Password successfully changed!"));
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured!"));
+  }
 }
 
 export async function handleUpdateSettings(req, res) {
-	try {
-		const { key, value } = req.body;
-		const userId = req.user._id;
+  try {
+    const { key, value } = req.body;
+    const userId = req.user._id;
 
-		await updatedSettings({ key, value, userId });
+    await updatedSettings({ key, value, userId });
 
-		res
-			.status(200)
-			.json(
-				responseModel(
-					true,
-					`${key.replace("_", " ")} settings updated successfully`
-				)
-			);
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured!"));
-	}
+    res
+      .status(200)
+      .json(
+        responseModel(
+          true,
+          `${key.replace("_", " ")} settings updated successfully`
+        )
+      );
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured!"));
+  }
 }
 
-export async function handleUserReport(req, res) {
-	try {
-		const userId = req.user._id;
+export async function handleUserPropertyPerformanceReport(req, res) {
+  try {
+    const userId = req.user._id;
 
-		const reports = await userReport(userId);
-	} catch (error) {
-		res
-			.status(error.status || 500)
-			.json(responseModel(false, error.message || "An error occured!"));
-	}
+    const reports = await userPropertyPerformanceReport(userId);
+
+    res
+      .status(200)
+      .json(
+        responseModel(
+          true,
+          `User Property performace reports fetched successfully`,
+          reports
+        )
+      );
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json(responseModel(false, error.message || "An error occured!"));
+  }
 }
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -2,65 +2,65 @@ import { Router } from "express";
 import validateMiddleware from "../middlewares/validation.middleware.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import {
-	handleCreateUser,
-	handleLoginUser,
-	handleUser,
-	handleRefreshToken,
-	handleVerifyUser,
-	handleUpdateUser,
-	handleChangePassword,
-	handleUpdateSettings,
-	handleUserReport,
+  handleCreateUser,
+  handleLoginUser,
+  handleUser,
+  handleRefreshToken,
+  handleVerifyUser,
+  handleUpdateUser,
+  handleChangePassword,
+  handleUpdateSettings,
+  handleUserPropertyPerformanceReport,
 } from "../controllers/user.controller.js";
 import {
-	createUserSchema,
-	loginUserSchema,
-	refreshTokenSchema,
-	verifyUserSchema,
-	updateUserSchema,
-	changePasswordSchema,
-	updateSettingsSchema,
+  createUserSchema,
+  loginUserSchema,
+  refreshTokenSchema,
+  verifyUserSchema,
+  updateUserSchema,
+  changePasswordSchema,
+  updateSettingsSchema,
 } from "../validations/user.validation.js";
 import upload from "../configs/multer.js";
 
 const userRouter = Router();
 
 userRouter.post(
-	"/create",
-	validateMiddleware(createUserSchema),
-	handleCreateUser
+  "/create",
+  validateMiddleware(createUserSchema),
+  handleCreateUser
 );
 userRouter.post("/login", validateMiddleware(loginUserSchema), handleLoginUser);
 userRouter.post(
-	"/refresh_token",
-	validateMiddleware(refreshTokenSchema),
-	handleRefreshToken
+  "/refresh_token",
+  validateMiddleware(refreshTokenSchema),
+  handleRefreshToken
 );
 userRouter.post(
-	"/verify",
-	validateMiddleware(verifyUserSchema),
-	handleVerifyUser
+  "/verify",
+  validateMiddleware(verifyUserSchema),
+  handleVerifyUser
 );
 
 userRouter.get("/", authMiddleware, handleUser);
 userRouter.put(
-	"/update",
-	authMiddleware,
-	upload.single("image"),
-	validateMiddleware(updateUserSchema),
-	handleUpdateUser
+  "/update",
+  authMiddleware,
+  upload.single("image"),
+  validateMiddleware(updateUserSchema),
+  handleUpdateUser
 );
 userRouter.post(
-	"/change_password",
-	authMiddleware,
-	validateMiddleware(changePasswordSchema),
-	handleChangePassword
+  "/change_password",
+  authMiddleware,
+  validateMiddleware(changePasswordSchema),
+  handleChangePassword
 );
 userRouter.patch(
-	"/settings",
-	authMiddleware,
-	validateMiddleware(updateSettingsSchema),
-	handleUpdateSettings
+  "/settings",
+  authMiddleware,
+  validateMiddleware(updateSettingsSchema),
+  handleUpdateSettings
 );
-userRouter.get("/reports", authMiddleware, handleUserReport);
+userRouter.get("/reports", authMiddleware, handleUserPropertyPerformanceReport);
 export default userRouter;
