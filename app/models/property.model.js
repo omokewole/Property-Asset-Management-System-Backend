6324ad8c3077205e2dd965f58fe61625e6bec93e
rasheed <demandwork247@gmail.com>
2024-11-22 03:47:59 +0100

    fixes

--- app/models/property.model.js
+++ app/models/property.model.js
@@ -1,60 +1,64 @@
 import mongoose from "mongoose";
 import TenantModel from "./tenant.model.js";
+import MaintenaceModel from "./maintenance.model.js";
+import NotificationModel from "./notification.model.js";
 
 const propertySchema = mongoose.Schema(
-	{
-		owner_id: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		location: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		street: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		unit_number: {
-			type: Number,
-			required: true,
-		},
-		attraction: {
-			type: String,
-			trim: true,
-		},
-		images_url: {
-			type: Array,
-			required: true,
-		},
-		description: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		property_type: {
-			type: String,
-			required: true,
-			enum: ["Residential", "Commercial"],
-		},
-	},
-	{ timestamps: true }
+  {
+    owner_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    street: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    unit_number: {
+      type: Number,
+      required: true,
+    },
+    attraction: {
+      type: String,
+      trim: true,
+    },
+    images_url: {
+      type: Array,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    property_type: {
+      type: String,
+      required: true,
+      enum: ["Residential", "Commercial"],
+    },
+  },
+  { timestamps: true }
 );
 
 propertySchema.pre("deleteOne", async function (next) {
-	try {
-		await TenantModel.deleteMany({ property_id: this._id });
-		next();
-	} catch (error) {
-		next(error);
-	}
+  try {
+    await TenantModel.deleteMany({ property_id: this._id });
+    await MaintenaceModel.deleteMany({ property: this._id });
+    await NotificationModel.deleteMany({ ref: this._id });
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const PropertyModel = mongoose.model("Property", propertySchema);
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -2,73 +2,84 @@ import mongoose from "mongoose";
 import PropertyModel from "./property.model.js";
 import TenantModel from "./tenant.model.js";
 import MaintenanceModel from "./maintenance.model.js";
+import NotificationModel from "./notification.model.js";
+import SettingsModel from "./settings.model.js";
 
 const userSchema = mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			trim: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		role: {
-			type: String,
-			enum: ["USER", "ADMIN"],
-			default: "USER",
-		},
-		phone: {
-			type: String,
-			default: "",
-		},
-		company: {
-			type: String,
-			default: null,
-		},
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    phone: {
+      type: String,
+      default: "",
+    },
+    company: {
+      type: String,
+      default: null,
+    },
 
-		image_url: {
-			type: String,
-			default: null,
-		},
-		verified_at: {
-			type: Date,
-			default: null,
-		},
-		email_token: {
-			type: String,
-			default: null,
-		},
-		email_token_expires: {
-			type: Date,
-			default: null,
-		},
-	},
-	{
-		timestamps: true,
-	}
+    image_url: {
+      type: String,
+      default: null,
+    },
+    verified_at: {
+      type: Date,
+      default: null,
+    },
+    email_token: {
+      type: String,
+      default: null,
+    },
+    email_token_expires: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-userSchema.pre("remove", async function (next) {
-	try {
-		await PropertyModel.deleteMany({ owner_id: this._id });
+userSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    const userId = this._id;
+    try {
+      await PropertyModel.deleteMany({ owner_id: userId });
 
-		await TenantModel.deleteMany({ owner_id: this._id });
+      await TenantModel.deleteMany({ owner_id: userId });
 
-		await MaintenanceModel.deleteMany({ owner_id: this._id });
-		
-		next();
-	} catch (error) {
-		next(error);
-	}
-});
+      await MaintenanceModel.deleteMany({ owner_id: userId });
+
+      await NotificationModel.deleteMany({ user_id: userId });
+
+      await SettingsModel.deleteMany({ user_id: userId });
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 const UserModel = mongoose.model("User", userSchema);
 
